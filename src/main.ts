2db6497420ea1ff6a31cf626497e976859d8e332
UDay <uday.dev.bd@gmail.com>
2023-07-20 19:37:22 +0600

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const configServcie = app.get(ConfigService);
+
+  const apiConfig = new DocumentBuilder()
+    .setTitle('Organick')
+    .setDescription('A ecommerce site')
+    .build();
+  const document = SwaggerModule.createDocument(app, apiConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(configServcie.get<number>('PORT'));
 }
 bootstrap();
