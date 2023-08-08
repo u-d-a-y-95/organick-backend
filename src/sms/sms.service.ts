24591499d6067c9fd450a13e1abb0657ae79f489
UDay <uday.dev.bd@gmail.com>
2023-08-09 01:43:08 +0600

    add sms service

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -2,6 +2,7 @@ import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Cache } from 'cache-manager';
+import { SignupDto } from 'src/auth/dtos/signup.dto';
 import { Twilio } from 'twilio';
 
 @Injectable()
@@ -10,24 +11,39 @@ export class SmsService {
     private configService: ConfigService,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
-  getOtp(digit = 6): number {
+  getOtp(digit = 6): string {
     let otp = '';
     for (let i = 0; i < digit; i++) {
       otp += String(Math.floor(Math.random() * 10));
     }
-    return Number(otp);
+    return otp;
   }
 
   async sendOtp(data) {
-    const otp = this.getOtp();
+    const mode = this.configService.get<string>('MODE');
+    const otpLength = this.configService.get<number>('SMS_OTP_LENGTH');
+    const otpTtl = this.configService.get<number>('OTP_TTL');
+    const otp = this.getOtp(otpLength);
     const message = `Your verificode is ${otp}`;
-    await this.cacheManager.set(`otp-${data.mobile}`, {
-      otp,
-      data,
-    });
-    this.sendSms(message, '+8801830546042' || data.mobile);
+    const mobile =
+      mode === 'DEVELOPMENT'
+        ? this.configService.get<string>('SMS_DEMO_NUMBER')
+        : data.mobile;
+
+    console.log(otpTtl, typeof otpTtl);
+    await this.cacheManager.set(
+      `otp-${data.mobile}`,
+      {
+        otp,
+        data,
+      },
+      Number(otpTtl),
+    );
+
+    // this.sendSms(message, mobile);
     return {
-      message: 'otp is send to the ',
+      mobile: data.mobile,
+      message: `otp is send to the ${mobile} - ${otp}`,
     };
   }
 
@@ -41,5 +57,22 @@ export class SmsService {
       to: mobile,
       body: message,
     });
+  }
+
+  async verifyOtp(mobile: string, otp: string) {
+    const cacheValue = await this.cacheManager.get<{
+      otp: string;
+      data: SignupDto;
+    }>(`otp-${mobile}`);
+    if (cacheValue.otp === otp) {
+      return {
+        status: 200,
+        data: cacheValue.data,
+      };
+    }
+    return {
+      status: 404,
+      message: `Otp is not correct`,
+    };
   }
 }
